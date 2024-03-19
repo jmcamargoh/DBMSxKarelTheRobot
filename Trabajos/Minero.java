1d3e870a25e3ab020f8a1604a87518bd16fab2af
Jose Manuel <jmcamargoh@eafit.edu.co>
2024-03-18 19:40:43 -0500

    Mineria Completamente Funcional

--- Trabajos/Minero.java
+++ Trabajos/Minero.java
@@ -11,6 +11,7 @@ public class Minero extends Robot implements Runnable {
     private static boolean primerMinero = true; // Variable para rastrear el primer minero
     private static int contador = 0;    // Cuanto se ha sacado de una sola mena
     private static int menas_acabadas = 0;  // Cuantas menas se han dejado ya vacias
+    private static int silo = 0; // Para la visualizacion por consola
 
     public Minero(int Street, int Avenue, Direction direction, int beepers, Color color, CountDownLatch minerosLatch) {
         super(Street, Avenue, direction, beepers, color);
@@ -94,62 +95,67 @@ public class Minero extends Robot implements Runnable {
         }
     }
 
-    public void minecraft() {
-        lock1.lock();
-        while (contador <= 40) {
-            if (contador < 40) {
-                try {
-                    if (primerMinero == false) {
-                        for (int i = 0; i < 20; i++) {
+    public void minar() {
+        while (contador <= 40) {   // Se pone la cantidad de menas que hay en cada silo (2000 en el ejercicio)
+            lock1.lock();   // Lock para la consistencia de los datos compartidos (menas_acabadas y contador)
+            silo = menas_acabadas+1;
+            try {
+                if (contador < 40) {   
+                    if (primerMinero == false) { // Cuando el primero entra y saca por primera vez del primer silo, solo se hace una vez en toda la ejecucion (por su posicion inicial)
+                        for (int i = 0; i < 20; i++) {  // Recoge su maxima capacidad (50 en el ejercicio)
                             pickBeeper();
                             contador++;
-                            System.out.println(contador);
+                            System.out.println("Beepers Extraidos: "+contador+" - Silo: "+silo);
                         }
                         cambioSentido();
-                        recto(1);
-                        primerMinero = true;
-                    } else {
-                        lock.lock();
+                        recto(1);   // Se ubica en el punto de entrega para dejar los beepers
+                        primerMinero = true;    // Habilita para que este caso no se vuelva a repetir
+                    } else {       // Todos los casos siguientes al primero
+                        lock.lock();    // Lock para el movimiento de los mineros (cuida que no se monten uno encima del otro)
                         try {
                             recto(1);
                             giroDerecha();
-                            for (int i = 0; i < 20; i++) {
+                            recto(menas_acabadas);  // Se posiciona en la mena actual a la que deben ir
+                            for (int i = 0; i < 20; i++) {  // Recoge su maxima capacidad (50 en el ejercicio)
                                 pickBeeper();
-                                contador++;
-                                System.out.println(contador);
+                                contador++; // La cantidad de beepers que han sido extraidos aumenta (para saber cuando debe cambiar de silo)
+                                System.out.println("Beepers Extraidos: "+contador+" - Silo: "+silo);
                             }
                             cambioSentido();
-                            recto(1);
+                            recto(menas_acabadas + 1);  // Se ubica en el punto de entrega para dejar los beepers
                         } finally {
                             lock.unlock();
                         }
                     }
-                } finally {
-                    lock1.unlock();
+                } else {
+                    menas_acabadas++;   // Si el silo actual ya se vacio
+                    contador = 0;   // Empieza de 0 para el nuevo silo
+                    if (menas_acabadas > 5) {   // En caso de que los silos queden vacios, los robots se apagan (por ahora)
+                        turnOff();  // Tiene un bug extraño que literal al apagarse uno, el otro no puede terminar su ejecucion
+                    }               // pero no acaba aqui el robot entonces por ahora no importa
                 }
+            } finally {
+                lock1.unlock();
+            }
     
-                if (anyBeepersInBeeperBag()) {
-                    for (int i = 0; i < 20; i++) {
-                        putBeeper();
-                    }
-                    giroIzquierda();
-                    recto(1);
-                    giroIzquierda();
-                    recto(1);
-                    giroIzquierda();
+            // Pone los beepers en el lugar de extraccion y acomoda al minero hasta que le liberen el lock y siga su trabajo
+            if (anyBeepersInBeeperBag()) {
+                for (int i = 0; i < 20; i++) {
+                    putBeeper();
                 }
-            } else {
-                // Aqui iria todo lo de pasar a otra mena, donde se debe reiniciar el contador y debe sumar 1 a menas_acabadas
-                // el problema es que no se porque no funciona correctamente la variable menas
-                // esa sirve para indicarle al robot que debe moverse un pasito mas a la derecha (siguiente mena)
-            }        
+                giroIzquierda();
+                recto(1);
+                giroIzquierda();
+                recto(1);
+                giroIzquierda();
+            }
         }
     }
 
     public void race() {
         // Las acciones del minero
         entrada();
-        minecraft();
+        minar();
     }
 
     @Override
