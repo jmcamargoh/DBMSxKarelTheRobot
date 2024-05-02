3ae6b60a0c45c97d9d36f6faff9128d88d1d552c
Jose Manuel <jmcamargoh@eafit.edu.co>
2024-05-01 19:03:11 -0500

    Primera Version del DBMS sin insercion automatica

---
+++ Main.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Primera interaccion con el usuario
+        System.out.println("Bienvenido al DBMS para KarelTheRobot!");
+        System.out.println("");
+        bienvenida(scan);
+    }
+    //-------------------------------------------------------------------------------------------------------------
+
+    // Metodo de interaccion general
+    public static void bienvenida(Scanner scan){
+        Tabla tabla = new Tabla(); // Instancia de la clase Tabla
+        ExecutorService executor = Executors.newFixedThreadPool(4); // Pool de threads para el paralelismo
+
+        System.out.println("Presione 1 para crear una nueva Base de Datos");
+        System.out.println("Presione 2 para ver las Bases de Datos existentes");
+        System.out.println("Presione 3 para agregar una fila a una de las tablas");
+        System.out.println("Presione 4 para ver el contenido de una de las tablas");
+        System.out.println("Presione 5 para hacer consultas");
+        System.out.print("Selecione: ");
+        String decision = scan.nextLine();
+        System.out.println("");
+
+        if (decision.equals("1")) {     // Crear nueva BD con sus Tablas
+            // Crear una BD como un folder
+            System.out.print("Ingrese el nombre de su nueva Base de Datos: ");
+            String nombre = scan.nextLine();
+            File db = new File("Bases de Datos/" + nombre);
+            if (db.mkdir()) {
+                String ruta = db.getPath();     // Ruta a la carpeta de la DB correpondiente
+                executor.execute(() -> tabla.generarTablaCSV("Robot", ruta));
+                executor.execute(() -> tabla.generarTablaCSV("LogEventos", ruta));
+                executor.execute(() -> tabla.generarTablaCSV("EstadoPrograma", ruta));
+                executor.execute(() -> tabla.generarTablaCSV("VariablesEstaticas", ruta));
+                executor.shutdown();    // Apagar el executor cuando todas las tareas hayan terminado
+                System.out.println("La Base de Datos '" + nombre + "' fue creada exitosamente!");
+            } else {
+                System.out.println("La Base de Datos '" + nombre + "' no se creó porque ya existe o hubo un error.");
+            }
+            System.out.println("");
+
+        } else if (decision.equals("2")) {      // Revisar las BD existentes en la carpeta
+            File dbs = new File("Bases de Datos");
+            String[] listado = dbs.list();
+            if (listado == null || listado.length == 0) {
+                System.out.println("No hay Bases de Datos creadas!");
+            } else {
+                System.out.println("Bases de Datos existentes:");
+                for (int i=0; i< listado.length; i++) {
+                    System.out.println(listado[i]);
+                }
+            }
+            System.out.println("");
+
+        } else if (decision.equals("3")) {  // Agregar fila (v1, pendiente de modificacion)
+            tabla.agregarFilaCSV("Robot", "Bases de Datos/Prueba");
+            System.out.println("");
+
+        } else if (decision.equals("4")) {  // Leer contenido completo
+            System.out.print("Ingrese el nombre de la Base de Datos deseada: ");
+            String bd = scan.nextLine();
+            System.out.print("Ingrese el nombre de la tabla a consultar: ");
+            String archivo = scan.nextLine();
+            String ruta = "Bases de Datos/" + bd + "/" + archivo + ".csv";
+
+            tabla.leerTablaCSV(ruta);
+            System.out.println("");
+        
+        } else if (decision.equals("5")) {  // Hacer consultas a las BD
+            System.out.print("Ingrese la base de datos de la que quiere consultar: ");
+            String bd = scan.nextLine();
+            System.out.print("Ingrese la tabla de la base de datos '" + bd + "' de la que quiere consultar: ");
+            String archivo = scan.nextLine();
+            String ruta = "Bases de Datos/" + bd+ "/" + archivo + ".csv";
+
+            System.out.print("Ingrese la columna a buscar: ");
+            String nombreColumna = scan.nextLine();
+            System.out.print("Ingrese el valor a buscar en la columna '" + nombreColumna + "': ");
+            String valorBuscado = scan.nextLine();
+            System.out.println("");
+
+            tabla.consultar(ruta, nombreColumna, valorBuscado);
+            System.out.println("");
+
+        } else {    // No supo acatar instrucciones :v
+            System.out.println("Comando incorrecto, ingrese un comando valido.");
+            System.out.println("");
+            bienvenida(scan);
+        }
+
+    }
+    //-------------------------------------------------------------------------------------------------------------
+}
---
+++ Tabla.java
@@ -0,0 +1,167 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Tabla {
+    private ArrayList<String> columnas;
+    private ArrayList<String> filas;    // Hay que ver como adaptar esto al codigo, aun es manual la insercion
+    private static final Lock lock = new ReentrantLock();
+
+    // Constructor
+    public Tabla() {
+        this.columnas = new ArrayList<>();
+        this.filas = new ArrayList<>();
+    }
+    //-------------------------------------------------------------------------------------------------------------
+
+    // Creacion de la tabla en CSV
+    public void generarTablaCSV(String nombre, String ruta){
+        try {
+            FileWriter writer = new FileWriter(ruta + "/" + nombre + ".csv");
+
+            lock.lock();
+            try {
+                // Las columnas de cada una de las tablas (se pueden agregar mas columnas dependiendo de los datos)
+                if (nombre.equals("Robot")) {
+                    columnas.add("idRobot");
+                    columnas.add("tipoRobot");
+                    columnas.add("encendido");
+
+                } else if (nombre.equals("LogEventos")) {
+                    columnas.add("idLogEventos");
+                    columnas.add("idRobot");
+                    columnas.add("timeStamp");
+                    columnas.add("avenida");
+                    columnas.add("calle");
+                    columnas.add("sirenas");
+
+                } else if (nombre.equals("EstadoPrograma")) {
+                    columnas.add("idEstadoPrograma");
+                    columnas.add("timeStamp");
+                    columnas.add("estado");
+
+                } else if (nombre.equals("VariablesEstaticas")) {
+                    columnas.add("idVariablesEstaticas");
+                    columnas.add("timeStamp");
+                }
+
+                // Escribir los nombres de las columnas como encabezados
+                for (int i=0; i < columnas.size(); i++) {
+                    writer.write(columnas.get(i));
+                    if (i < columnas.size()) {
+                        writer.write(",");
+                    }
+                }
+                writer.write("\n");
+                columnas.clear();
+            } finally {
+                lock.unlock();
+            }
+
+            writer.close();
+            System.out.println("Archivo CSV para la tabla '" + nombre + "' ha sido generado");
+        } catch (IOException e) {
+            System.out.println("Error al generar el archivo CSV: " + e.getMessage());
+        }
+    }
+    //-------------------------------------------------------------------------------------------------------------
+
+    // Insertar una fila en la tabla (se debe modificar)
+    public void agregarFilaCSV(String nombre, String ruta){
+        try {
+            FileWriter fileWriter = new FileWriter(ruta + "/" + nombre + ".csv", true);
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+
+            // Agregar la nueva fila al final del archivo *(ejemplo para tabla Robot)
+            String nuevaFila = "1,2,true,"; // Reemplaza con los valores que se desean agregar
+            writer.write(nuevaFila);
+            writer.newLine(); // Agrega una nueva línea después de la fila
+
+            // Cerrar el escritor
+            writer.close();
+
+            System.out.println("Se agregó una nueva fila al archivo CSV.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //-------------------------------------------------------------------------------------------------------------
+
+    // Traer toda la informacion de la tabla
+    public void leerTablaCSV(String ruta){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(ruta));
+            String linea;
+
+            System.out.println("");
+            System.out.println("Resultado: ");
+
+            // Leer cada línea del archivo y mostrarla en la consola
+            while ((linea = reader.readLine()) != null) {
+                System.out.println(linea);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //-------------------------------------------------------------------------------------------------------------
+
+    // Consultas por valor de columna
+    public void consultar(String ruta, String nombreColumna, String valorBuscado){
+        List<String[]> resultados = new ArrayList<>();
+        List<String> columnas = null;
+        int indiceColumna = -1;
+        
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(ruta));
+            String linea;
+
+            // Leer la primera fila para obtener los nombres de las columnas
+            if ((linea = reader.readLine()) != null) {
+                columnas = Arrays.asList(linea.split(","));
+                indiceColumna = columnas.indexOf(nombreColumna);
+            }
+
+            // Si el nombre de la columna existe en esa tabla
+            if (indiceColumna != -1){
+                // Leer cada fila del archivo
+                while ((linea = reader.readLine()) != null) {
+                    // Dividir la fila en sus partes usando la coma como delimitador
+                    String[] partes = linea.split(",");
+
+                    // Si la fila corresponde con el valor buscado
+                    if (partes.length > indiceColumna && partes[indiceColumna].equals(valorBuscado)) {
+                        resultados.add(partes);
+                    }
+                }
+            } else {
+                System.out.println("La columna ingresada no existe en la tabla");
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        // Imprimir resultados
+        if (!resultados.isEmpty()) {
+            System.out.println("Resultados Encontrados para la columna '" + nombreColumna + "' con valor de '" + valorBuscado + "':");
+            for (String[] fila : resultados) {
+                for (String dato : fila) {
+                    System.out.print(dato + ",");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("No se encontraron resultados");
+        }
+    }
+    //-------------------------------------------------------------------------------------------------------------
+}
